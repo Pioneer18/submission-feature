151b4c98fd9255f818d2aad1a6a761b03a2381a3
Jonathan Sells <sellsj14@gmail.com>
2019-01-31 11:43:14 -0500

    required all sub-interfaces (directories) into the index.js of the main interface

---
+++ src/interface/index.js
@@ -0,0 +1,6 @@
+const auth = require('./auth');
+const automation_calc = require('./automation-calc');
+const common = require('./common');
+const logs_notifications = require('./logs-notifications');
+const mapper = require('./mapper');
+const mongo = require('./mongoDB');
